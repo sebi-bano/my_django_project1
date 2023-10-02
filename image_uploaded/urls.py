fd9b956b67ef854fd8eba0fcecc81c3071334f24
sebi bano <sebiraeen.700@gmail.com>
2023-10-02 11:18:50 +0530

    django image uploader website

--- image_uploaded/urls.py
+++ image_uploaded/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('image_upload/',views.imageUpload)
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+print(static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT))
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Image(models.Model):
+    photo= models.ImageField(upload_to="myimage")
+    date= models.DateTimeField(auto_now_add=True)
